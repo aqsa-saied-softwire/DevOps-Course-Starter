4b6254f4b9fac12d8906ffd7fe4dff440d1c30af
Aqsa Saied <aqsa.saied@softwire.com>
2024-01-16 16:52:55 +0000

    can add

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template
-from todo_app.data.session_items import get_items
+from flask import Flask, render_template, request, redirect
+from todo_app.data.session_items import get_items, add_item
 from todo_app.flask_config import Config
 
 app = Flask(__name__)
@@ -10,3 +10,9 @@ app.config.from_object(Config())
 def index():
     items = get_items()
     return render_template('index.html',items=items)
+
+@app.route('/add', methods=['POST'])
+def submit_film():
+    title = request.form['title']
+    add_item(title)
+    return redirect('/')
