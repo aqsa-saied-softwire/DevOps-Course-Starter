11e37d3cc7cb155b2855221932ff9329f57b95a3
Aqsa Saied <aqsa.saied@softwire.com>
2024-02-07 17:22:42 +0000

    can update status

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, request, redirect
-from todo_app.data.session_items import get_items, add_item
 from todo_app.flask_config import Config
-from todo_app.data.trello_items import trello_get_items
+from todo_app.data.trello_items import trello_get_items, trello_add_item, change_to_doing, change_to_done
 
 app = Flask(__name__)
 app.config.from_object(Config())
@@ -15,5 +14,17 @@ def index():
 @app.route('/add', methods=['POST'])
 def submit_film():
     title = request.form['title']
-    add_item(title)
+    trello_add_item(title)
+    return redirect('/')
+
+@app.route('/update-doing', methods=['POST'])
+def update_item_to_doing():
+    id = request.form['id']
+    change_to_doing(id)
+    return redirect('/')
+
+@app.route('/update-done', methods=['POST'])
+def update_item_to_done():
+    id = request.form['id']
+    change_to_done(id)
     return redirect('/')
--- todo_app/data/trello_items.py
+++ todo_app/data/trello_items.py
@@ -3,19 +3,22 @@ import requests, json, os
 board_id = os.getenv('BOARD_ID')
 api_key = os.getenv('TRELLO_API_KEY')
 token = os.getenv('TRELLO_API_TOKEN')
+to_do_list = os.getenv('TO_DO_LIST_ID')
+doing_list = os.getenv('DOING_LIST_ID')
+done_list = os.getenv('DONE_LIST_ID')
 
-url = 'https://api.trello.com/1/boards/{board_id}/lists'.format(board_id=board_id)
-headers = {
-    "Accept": "*/*"
-}
-
-query = {
-    'key': api_key,
-    'token': token,
-    'cards': 'open'
-}
 
 def trello_get_items():
+    url = 'https://api.trello.com/1/boards/{board_id}/lists'.format(board_id=board_id)
+    headers = {
+        "Accept": "*/*"
+    }
+
+    query = {
+        'key': api_key,
+        'token': token,
+        'cards': 'open'
+    }
     response = requests.request(
         "GET",
         url,
@@ -28,5 +31,67 @@ def trello_get_items():
         status = list["name"]
         tasks = list["cards"]
         for task in tasks:
-            items.append({ 'id': task['idShort'], 'status': status, 'title': task['name']})
+            items.append({ 'id': task['id'], 'status': status, 'title': task['name']})
     return items
+
+def trello_add_item(title):
+    url = 'https://api.trello.com/1/cards'
+    headers = {
+        "Accept": "*/*"
+    }
+
+    query = {
+        'key': api_key,
+        'token': token,
+        'name': title,
+        'idList': to_do_list
+    }
+
+    requests.request(
+        "POST",
+        url,
+        headers=headers,
+        params=query
+    )
+
+def change_to_doing(item):
+    url = 'https://api.trello.com/1/cards/{id}'.format(id=item)
+    headers = {
+        "Accept": "*/*"
+    }
+    print(item)
+
+    query = {
+        'key': api_key,
+        'token': token,
+        'idList': doing_list
+    }
+
+    response = requests.request(
+        "PUT",
+        url,
+        headers=headers,
+        params=query
+    )
+    print(response.reason)
+
+def change_to_done(item):
+    url = 'https://api.trello.com/1/cards/{id}'.format(id=item)
+    headers = {
+        "Accept": "*/*"
+    }
+    print(item)
+
+    query = {
+        'key': api_key,
+        'token': token,
+        'idList': done_list
+    }
+
+    response = requests.request(
+        "PUT",
+        url,
+        headers=headers,
+        params=query
+    )
+    print(response.reason)
